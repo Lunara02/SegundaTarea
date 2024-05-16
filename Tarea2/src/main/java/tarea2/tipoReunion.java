45679127c2e2982f88bb07963105db27c9a9a387
blokkxz_ <166780700+Lunara02@users.noreply.github.com>
2024-05-16 11:08:21 -0400

    Documentacion tipoReunion
    se ha implementado la documentacion a el enum tipoReunion

--- Tarea2/src/main/java/tarea2/tipoReunion.java
+++ Tarea2/src/main/java/tarea2/tipoReunion.java
@@ -1,7 +1,22 @@
 package tarea2;
-
+/**
+ * La enumeración tipoReunion define los tipos de reunión disponibles.
+ * Puede ser utilizada para clasificar las reuniones según su propósito o tema.
+ * Los valores posibles son TECNICA, MARKETING y OTRO.
+ *
+ * @author Cristobal Gonzalez
+ */
 public enum tipoReunion {
+    /**
+     * Indica que la reunión es de tipo Técnica.
+     */
     TECNICA,
+    /**
+     * Indica que la reunión es de tipo Marketing.
+     */
     MARKETING,
+    /**
+     * Indica que la reunión es de tipo Otro.
+     */
     OTRO
 }
