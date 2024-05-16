8135b5c2ec3571bfe1a66285d99b2cc9203121ae
Sebavegs2004 <sevega2023@inf.udec.cl>
2024-05-16 04:21:30 -0400

    Fix + Creacion Constructor

--- Tarea2/src/main/java/tarea2/ReunionPresencial.java
+++ Tarea2/src/main/java/tarea2/ReunionPresencial.java
@@ -1,5 +1,11 @@
-package org.example;
+package tarea2;
 import Excepciones.*;
 
 public class ReunionPresencial extends Reunion {
     private String sala;
+
+    public ReunionPresencial(int duracionPrevista, String organizador, Invitacion listaInvitados, String sala, tipoReunion tipoReunion) throws noInvitados{
+        super(duracionPrevista, organizador, listaInvitados, sala, tipoReunion);
+        this.sala = sala;
+    }
+}
