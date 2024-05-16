eebe2d4e8a47d24b811732c0661fb0764da91542
blokkxz_ <166780700+Lunara02@users.noreply.github.com>
2024-05-16 11:14:14 -0400

    Documentacion ReunionTest
    se ha implementado la documentacion de ReunionTest

--- Tarea2/src/test/java/tarea2/ReunionTest.java
+++ Tarea2/src/test/java/tarea2/ReunionTest.java
@@ -5,18 +5,26 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
-
+/**
+ * Clase de prueba para la clase Reunion.
+ */
 class ReunionTest {
     private Empleado emp1;
     private Invitacion Lista;
     private ReunionPresencial ReunionP;
-
+    /**
+     * Configuración inicial para cada prueba.
+     */
     @BeforeEach
     void setUp() {
         emp1 = new Empleado("200404", "Pedro", "Perez", "PePe@hotmail.com");
         Lista = new Invitacion();
     }
-
+    /**
+     * Prueba para verificar el manejo de una lista de invitados vacía al crear una reunión presencial.
+     *
+     * @throws noInvitados si no se invitan empleados a la reunión.
+     */
     @Test
     @DisplayName("Lista invitados vacia")
     void InvitadosVacio() {
@@ -26,12 +34,22 @@ class ReunionTest {
             assertTrue(true);
         }
     }
+    /**
+     * Prueba para verificar el manejo de una lista de invitados no vacía al crear una reunión presencial.
+     *
+     * @throws noInvitados si no se invitan empleados a la reunión.
+     */
     @Test
     @DisplayName("Lista invitados no vacia")
     void Invitadosnovacio() throws noInvitados {
         Lista.invitar(emp1);
         ReunionP = new ReunionPresencial(4,"Pedro",Lista, "Reunion 1", tipoReunion.MARKETING);
     }
+    /**
+     * Prueba para verificar el manejo de una fecha y hora pasadas al establecer la fecha y hora de la reunión.
+     *
+     * @throws noInvitados si no se invitan empleados a la reunión.
+     */
     @Test
     @DisplayName("Fecha Pasada")
     void Fechapasada() throws noInvitados{
@@ -43,6 +61,12 @@ class ReunionTest {
             assertTrue(true);
         }
     }
+    /**
+     * Prueba para verificar el manejo de una fecha y hora válidas al establecer la fecha y hora de la reunión.
+     *
+     * @throws noInvitados si no se invitan empleados a la reunión.
+     * @throws fechaPasada si la fecha y hora establecidas están en el pasado.
+     */
     @Test
     @DisplayName("Fecha valida")
     void Fechavalida() throws noInvitados, fechaPasada{
