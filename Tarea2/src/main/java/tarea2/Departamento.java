460b2ebd0e64abd15989cff552c517f43d0344be
blokkxz_ <166780700+Lunara02@users.noreply.github.com>
2024-05-16 02:14:21 -0400

    Documentacion Departamento
    se agrego la documentacion de Departamento

--- Tarea2/src/main/java/tarea2/Departamento.java
+++ Tarea2/src/main/java/tarea2/Departamento.java
@@ -1,25 +1,54 @@
 package tarea2;
 
 import java.util.ArrayList;
-
+/**
+ * La clase Departamento representa un departamento cualquiera.
+ * Contiene una lista de empleados que trabajan en ese departamento.
+ *
+ * @author Cristobal Gonzalez
+ */
 public class Departamento {
     private String nombre;
     private ArrayList<Empleado> empleados;
-
+    /**
+     * Constructor para la clase Departamento.
+     *
+     * @param nombre nombre del departamento.
+     */
     public Departamento(String nombre) {
         this.nombre = nombre;
         this.empleados = new ArrayList<>();
     }
+    /**
+     * Agrega un empleado al departamento.
+     *
+     * @param empleado empleado a agregar.
+     */
     public void addEmpleado(Empleado empleado) {
         empleados.add(empleado);
         empleado.setDepartamento(getNombre());
     }
+    /**
+     * Getter del nombre del departamento.
+     *
+     * @return nombre del departamento.
+     */
     public String getNombre() {
         return nombre;
     }
+    /**
+     * Getter de la cantidad de empleados en el departamento.
+     *
+     * @return cantidad de empleados en el departamento.
+     */
     public int getCantidadEmpleado() {
         return empleados.size();
     }
+    /**
+     * Getter de la lista de empleados en el departamento.
+     *
+     * @return lista de empleados en el departamento.
+     */
     public ArrayList<Empleado> getEmpleados() {
         return empleados;
     }
