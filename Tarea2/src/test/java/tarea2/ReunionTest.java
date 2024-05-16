b503d0ddc6e2b80704380214f63c803e28ef8186
brai44 <braian.urba@gmail.com>
2024-05-16 05:12:38 -0400

    Creacion TestInvitadoVacio
    Verificar el caso de que no hay invitados a la reunion

--- Tarea2/src/test/java/tarea2/ReunionTest.java
+++ Tarea2/src/test/java/tarea2/ReunionTest.java
@@ -9,11 +9,21 @@ import static org.junit.jupiter.api.Assertions.*;
 class ReunionTest {
     private Empleado emp1;
     private Invitacion Lista;
-    private ReunionPresencial reunionP;
+    private ReunionPresencial ReunionP;
 
     @BeforeEach
     void setUp() {
-        emp1 = new Empleado("id2", "Lavalosa", "goku","hmry");
+        emp1 = new Empleado("200404", "Pedro", "Perez", "PePe@hotmail.com");
         Lista = new Invitacion();
+    }
+
+    @Test
+    @DisplayName("Lista invitados vacia")
+    void InvitadosVacio() {
+        try {
+            ReunionP = new ReunionPresencial(4,"Pedro",Lista, "Reunion 1", tipoReunion.MARKETING);
+        } catch (noInvitados e){
+            assertTrue(true);
+        }
     }
 }
