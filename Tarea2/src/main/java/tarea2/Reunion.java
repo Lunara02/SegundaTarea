16b3838052d51c9d62b7cebf4c9b3193bc874ef1
Sebavegs2004 <sevega2023@inf.udec.cl>
2024-05-16 03:46:04 -0400

    Creacion Constructor Reunion

--- Tarea2/src/main/java/tarea2/Reunion.java
+++ Tarea2/src/main/java/tarea2/Reunion.java
@@ -18,4 +18,16 @@ abstract class Reunion {
     private Asistencia asistencia;
     private String TIPO;
     private ArrayList<Empleado> Lista;
+
+    public Reunion(int duracionPrevista, String Organizador, Invitacion Invitados, String NombreSala, tipoReunion A) throws noInvitados{
+        this.duracionPrevista = Duration.ofHours(duracionPrevista);
+        this.Organizador = Organizador;
+        this.NombreSala = NombreSala;
+        if(Invitados.getInvitados().size() == 0){
+            throw new noInvitados();
+        }
+        this.asistencia = new Asistencia(Invitados.getInvitados());
+        this.TIPO = A.toString();
+        this.Lista = Invitados.getInvitados();
+    }
 }
