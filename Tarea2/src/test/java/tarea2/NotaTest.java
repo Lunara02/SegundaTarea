e03d17af3ab0a1330c8a04dcf9bcab6f968d182d
brai44 <braian.urba@gmail.com>
2024-05-16 04:43:06 -0400

    Feat NotaTest
    Declarar variables para el test

--- Tarea2/src/test/java/tarea2/NotaTest.java
+++ Tarea2/src/test/java/tarea2/NotaTest.java
@@ -1,7 +1,15 @@
 package tarea2;
 
+import Excepciones.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
-
 class NotaTest {
-
+    private Empleado emp1;
+    private Empleado emp2;
+    private Empleado emp3;
+    private Invitacion Lista;
+    private ReunionPresencial ReunionP;
 }
