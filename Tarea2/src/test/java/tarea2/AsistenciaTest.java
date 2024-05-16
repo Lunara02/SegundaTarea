08888a9261cd8cde2897920e436dd3b80dc62edf
brai44 <braian.urba@gmail.com>
2024-05-16 04:35:08 -0400

    feat: Asistencia Test
    Creacion setUp que inicializara las variables antes de cada test

--- Tarea2/src/test/java/tarea2/AsistenciaTest.java
+++ Tarea2/src/test/java/tarea2/AsistenciaTest.java
@@ -1,8 +1,23 @@
 package tarea2;
 
+import Excepciones.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
 class AsistenciaTest {
     private Empleado emp1;
     private Empleado emp2;
     private Invitacion Lista;
     private ReunionPresencial ReunionP;
+
+    @BeforeEach
+    void setUp() throws noInvitados {
+        emp1 = new Empleado("id1", "nombre1", "apellido1", "correo1");
+        emp2 = new Empleado("id2", "Lavalosa", "goku","hmry");
+        Lista = new Invitacion();
+        Lista.invitar(emp2);
+        ReunionP = new ReunionPresencial(4,"Pedro",Lista, "Reunion 1", tipoReunion.MARKETING);
+    }
 }
