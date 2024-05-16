885561b38fad3b88f0090f19f35f71372241834e
blokkxz_ <166780700+Lunara02@users.noreply.github.com>
2024-05-16 11:13:33 -0400

    Documentacion AsistenciaTest
    se ha implementado la documentacion de asistenciaTest

--- Tarea2/src/test/java/tarea2/AsistenciaTest.java
+++ Tarea2/src/test/java/tarea2/AsistenciaTest.java
@@ -6,12 +6,19 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
+/**
+ * Clase de prueba para la clase Asistencia.
+ */
 class AsistenciaTest {
     private Empleado emp1;
     private Empleado emp2;
     private Invitacion Lista;
     private ReunionPresencial ReunionP;
-
+    /**
+     * Configuración inicial para cada prueba.
+     *
+     * @throws noInvitados si ocurre un error al invitar a los empleados.
+     */
     @BeforeEach
     void setUp() throws noInvitados {
         emp1 = new Empleado("id1", "nombre1", "apellido1", "correo1");
@@ -20,6 +27,9 @@ class AsistenciaTest {
         Lista.invitar(emp2);
         ReunionP = new ReunionPresencial(4,"Pedro",Lista, "Reunion 1", tipoReunion.MARKETING);
     }
+    /**
+     * Prueba para verificar el manejo de la llegada de un empleado no invitado.
+     */
     @Test
     @DisplayName("LLego empleado no invitado")
     void TestNoInvitados() {
@@ -30,6 +40,11 @@ class AsistenciaTest {
             assertTrue(true);
         }
     }
+    /**
+     * Prueba para verificar el manejo de la llegada de un empleado invitado.
+     *
+     * @throws noInvitado si ocurre un error al registrar la llegada del empleado.
+     */
     @Test
     @DisplayName("LLego empleado invitado")
     void TestInvitado() throws noInvitado{
