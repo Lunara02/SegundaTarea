e5bcb45e3702a1c190d41f1b82553e124fc9ed3e
blokkxz_ <166780700+Lunara02@users.noreply.github.com>
2024-05-16 00:57:59 -0400

    Implementacion Asitencia
    se implemento la clase Asistencia con sus parametros y Constructor ademas de los Add para llenar las listas

--- Tarea2/src/main/java/tarea2/Asistencia.java
+++ Tarea2/src/main/java/tarea2/Asistencia.java
@@ -1,4 +1,38 @@
 package tarea2;
 
+
+import java.time.Instant;
+import java.util.ArrayList;
+
 public class Asistencia {
+    private ArrayList<Empleado> arrAsistencias;
+    private ArrayList<Empleado> arrAusencias;
+    private ArrayList<Empleado> arrRetrasos;
+    private int arrInvitados;
+
+    public Asistencia(ArrayList<Empleado> arrInvitados) {
+        this.arrAusencias = arrInvitados;
+        this.arrAsistencias = new ArrayList<>();
+        this.arrRetrasos = new ArrayList<>();
+        this.arrInvitados = arrInvitados.size();
+    }
+
+    public void newAsistente(Empleado e, Instant horaInicio, Instant horaFin) {
+        if (horaInicio == null || e.getHoraLlegada().equals(horaInicio)) {
+            addAsistencia(e);
+        } else if (e.getHoraLlegada().isAfter(horaInicio) && horaFin == null) {
+            addRetraso(e);
+        }
+    }
+
+    public void addAsistencia(Empleado e) {
+        arrAusencias.remove(e);
+        arrAsistencias.add(e);
+    }
+
+    public void addRetraso(Empleado e) {
+        arrAusencias.remove(e);
+        arrRetrasos.add(e);
+    }
+
 }
