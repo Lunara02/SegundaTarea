4fe6c1d6be353ccc11e1be16c733fcda8a291e7a
Sebavegs2004 <sevega2023@inf.udec.cl>
2024-05-16 04:18:46 -0400

    Fix ReunionPresencial

--- Tarea2/src/main/java/tarea2/ReunionPresencial.java
+++ Tarea2/src/main/java/tarea2/ReunionPresencial.java
@@ -1,6 +1,12 @@
 package tarea2;
 import Excepciones.*;
 
-public class ReunionVirtual extends Reunion {
-    private String enlace;
+/**
+ * La clase ReunionPresencial representa una reunión presencial.
+ * Extiende la clase Reunion e incluye información adicional sobre la sala donde se llevará a cabo la reunión.
+ *
+ * @author Cristobal Gonzalez
+ */
+public class ReunionPresencial extends Reunion {
+    private String sala;
 }
