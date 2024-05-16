be9a40a868e54f4435f103c570f7cba1e2a66d1e
Sebavegs2004 <sevega2023@inf.udec.cl>
2024-05-16 03:43:41 -0400

    Creacion Variables Reunion
    Se crean variables private ne reunion

--- Tarea2/src/main/java/tarea2/Reunion.java
+++ Tarea2/src/main/java/tarea2/Reunion.java
@@ -1,4 +1,21 @@
 package tarea2;
 
-public class Reunion {
+import Excepciones.*;
+import java.time.Duration;
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.Date;
+
+abstract class Reunion {
+    private Date fecha;
+    private Instant horaPrevista = null;
+    private Duration duracionPrevista;
+    private Instant horaInicio = null;
+    private Instant horaFin = null;
+    private String Organizador;
+    private String NombreSala;
+    private String Contenido = new String("");
+    private Asistencia asistencia;
+    private String TIPO;
+    private ArrayList<Empleado> Lista;
 }
