9901a6e6bb361605379944e5f76681b2190f1774
Sebavegs2004 <sevega2023@inf.udec.cl>
2024-05-16 04:19:20 -0400

    Revert "Fix ReunionPresencial"
    This reverts commit 4fe6c1d6be353ccc11e1be16c733fcda8a291e7a.

--- Tarea2/src/main/java/tarea2/ReunionPresencial.java
+++ Tarea2/src/main/java/tarea2/ReunionPresencial.java
@@ -1,12 +1,6 @@
 package tarea2;
 import Excepciones.*;
 
-/**
- * La clase ReunionPresencial representa una reunión presencial.
- * Extiende la clase Reunion e incluye información adicional sobre la sala donde se llevará a cabo la reunión.
- *
- * @author Cristobal Gonzalez
- */
-public class ReunionPresencial extends Reunion {
-    private String sala;
+public class ReunionVirtual extends Reunion {
+    private String enlace;
 }
