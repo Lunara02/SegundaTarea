0751a1e2e5a0d122651cfeee7ab4d1213a15b89d
blokkxz_ <166780700+Lunara02@users.noreply.github.com>
2024-05-16 11:07:54 -0400

    Documentacion ReunionVirtual
    se ha implementado la documentacion de ReunionVirtual

--- Tarea2/src/main/java/tarea2/ReunionVirtual.java
+++ Tarea2/src/main/java/tarea2/ReunionVirtual.java
@@ -1,14 +1,32 @@
 package tarea2;
 import Excepciones.*;
-
+/**
+ * La clase ReunionVirtual representa una reunión virtual.
+ * Extiende la clase Reunion e incluye información adicional sobre el enlace utilizado para la reunión.
+ *
+ * @author Cristobal Gonzalez
+ */
 public class ReunionVirtual extends Reunion {
     private String enlace;
-
+    /**
+     * Constructor para la clase ReunionVirtual.
+     *
+     * @param duracionPrevista Duración prevista de la reunión en minutos.
+     * @param organizador organizador de la reunión.
+     * @param listaInvitados La lista de invitados a la reunión.
+     * @param enlace enlace utilizado para la reunión virtual.
+     * @param tipoReunion tipo de reunión (presencial o virtual).
+     * @throws Exception si ocurre un error al crear la reunión.
+     */
     public ReunionVirtual(int duracionPrevista, String organizador, Invitacion listaInvitados, String enlace, tipoReunion tipoReunion) throws noInvitados {
         super(duracionPrevista, organizador, listaInvitados, enlace, tipoReunion);
         this.enlace = enlace;
     }
-
+    /**
+     * Obtiene el enlace utilizado para la reunión virtual.
+     *
+     * @return enlace de la reunión virtual.
+     */
     public String getEnlace() {
         return this.enlace;
     }
