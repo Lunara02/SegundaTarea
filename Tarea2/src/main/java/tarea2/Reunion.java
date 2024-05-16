e3f75cbbf82c0238e9e76d218358ddf866d12a7a
Sebavegs2004 <sevega2023@inf.udec.cl>
2024-05-16 03:58:23 -0400

    Implementacion getTotalAsistencias
    Retorna un entero con la cantidad de asistencias incluyendo los que llegaron a tiempo y los atrasados

--- Tarea2/src/main/java/tarea2/Reunion.java
+++ Tarea2/src/main/java/tarea2/Reunion.java
@@ -53,4 +53,8 @@ abstract class Reunion {
     public ArrayList<Empleado> getRetrasos() {
         return asistencia.getRetrasos();
     }
+
+    public int getTotalAsistencias() {
+        return getAsistencias().size() + getRetrasos().size();
+    }
 }
