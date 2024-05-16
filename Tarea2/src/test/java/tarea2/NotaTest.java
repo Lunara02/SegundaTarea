a1b00adacdfe21cc3580145900e004384160194e
brai44 <braian.urba@gmail.com>
2024-05-16 04:46:28 -0400

    Feat NotaTest
    Creacion setUp que inicializara las variables antes de cada test

--- Tarea2/src/test/java/tarea2/NotaTest.java
+++ Tarea2/src/test/java/tarea2/NotaTest.java
@@ -12,4 +12,16 @@ class NotaTest {
     private Empleado emp3;
     private Invitacion Lista;
     private ReunionPresencial ReunionP;
+
+    @BeforeEach
+    void setUp() throws noInvitados {
+        emp1 = new Empleado("200404", "Pedro", "Perez", "PePe@hotmail.com");
+        emp2 = new Empleado("ID2345", "Jose", "Vera","JoVe20@gmail.com");
+        emp3 = new Empleado("123456", "Juan", "Campos","JuCa@hotmail.com");
+        Lista = new Invitacion();
+        Lista.invitar(emp2);
+        Lista.invitar(emp1);
+        Lista.invitar(emp3);
+        ReunionP = new ReunionPresencial(4,"Pedro",Lista, "Reunion 1", tipoReunion.MARKETING);
+    }
 }
