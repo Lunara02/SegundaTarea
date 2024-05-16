32328bf15df88b680431974deebcaaa813f5edd9
blokkxz_ <166780700+Lunara02@users.noreply.github.com>
2024-05-16 00:50:40 -0400

    Implementacion Empleado
    Se definen los parametros y el constructor de Empleado

--- Tarea2/src/main/java/tarea2/Empleado.java
+++ Tarea2/src/main/java/tarea2/Empleado.java
@@ -1,4 +1,20 @@
 package tarea2;
 
+import java.time.Instant;
+
 public class Empleado {
+    private String id;
+    private String apellido;
+    private String nombre;
+    private String correo;
+    private Instant horaLlegada = null;
+    private String Dep;
+
+    public Empleado(String id, String nombre, String apellido, String correo) {
+        this.id = id;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.correo = correo;
+    }
+
 }
