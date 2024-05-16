59ae2aa8e0fe3603587cd57909b08dae43fbe1f4
blokkxz_ <166780700+Lunara02@users.noreply.github.com>
2024-05-16 00:54:38 -0400

    Implementacion Invitacion
    se agrego la implementación de Invitacion, sus parametros y para añadir empleados o departamento

--- Tarea2/src/main/java/tarea2/Invitacion.java
+++ Tarea2/src/main/java/tarea2/Invitacion.java
@@ -1,4 +1,20 @@
 package tarea2;
 
+import java.time.Instant;
+import java.util.ArrayList;
+
 public class Invitacion {
+    private Instant hora;
+    private ArrayList<Empleado> invitados;
+
+    public Invitacion() {
+        invitados = new ArrayList<>();
+    }
+    public void invitar(Empleado e) {
+        invitados.add(e);
+    }
+    public void invitar(Departamento d) {
+        invitados.addAll(d.getEmpleados());
+    }
+
 }
