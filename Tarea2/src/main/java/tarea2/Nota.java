f2240b5a7f0b3d12fda1d108609c5cf4da4a6996
blokkxz_ <166780700+Lunara02@users.noreply.github.com>
2024-05-16 11:07:08 -0400

    Documentacion Nota
    se implemento la documentacion de la clase Nota

--- Tarea2/src/main/java/tarea2/Nota.java
+++ Tarea2/src/main/java/tarea2/Nota.java
@@ -2,18 +2,28 @@ package tarea2;
 
 import java.io.FileWriter;
 import java.io.IOException;
-
+/**
+ * La clase Nota representa una nota que se puede guardar en un archivo de texto.
+ * Al crear una instancia de Nota, se guarda el contenido de la nota en un archivo con el nombre especificado.
+ * Si ocurre un error durante la escritura del archivo, se maneja imprimiendo el mensaje de error en la consola.
+ *
+ * @author Cristobal Gonzalez
+ */
 public class Nota {
     private String contenido;
     private String nombreArchivo;
-
+    /**
+     * Constructor para la clase Nota.
+     *
+     * @param contenido El contenido de la nota a guardar.
+     * @param nombreArchivo El nombre del archivo en el que se guardará la nota.
+     */
     public Nota(String contenido, String nombreArchivo) {
         try {
             FileWriter nota = new FileWriter(nombreArchivo);
             nota.write(contenido);
             nota.close();
         } catch (IOException a) {
-            // Si ocurre un error durante la escritura del archivo, se imprime el mensaje de error en la consola.
             a.getMessage();
             a.printStackTrace();
         }
