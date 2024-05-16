77aba3a6844e69a65bf1612f4600746401124eb4
Sebavegs2004 <sevega2023@inf.udec.cl>
2024-05-16 04:25:26 -0400

    Creacion Constructor ReunionVirtual

--- Tarea2/src/main/java/tarea2/ReunionVirtual.java
+++ Tarea2/src/main/java/tarea2/ReunionVirtual.java
@@ -3,4 +3,9 @@ import Excepciones.*;
 
 public class ReunionVirtual extends Reunion {
     private String enlace;
+
+    public ReunionVirtual(int duracionPrevista, String organizador, Invitacion listaInvitados, String enlace, tipoReunion tipoReunion) throws noInvitados {
+        super(duracionPrevista, organizador, listaInvitados, enlace, tipoReunion);
+        this.enlace = enlace;
+    }
 }
