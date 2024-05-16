d7db0b4c6a92ba9c78b1f4e084f2163624806764
blokkxz_ <166780700+Lunara02@users.noreply.github.com>
2024-05-16 00:53:02 -0400

    Departamento Getter&Setter
    se agregaron los getter y setters a Departamento

--- Tarea2/src/main/java/tarea2/Departamento.java
+++ Tarea2/src/main/java/tarea2/Departamento.java
@@ -10,5 +10,17 @@ public class Departamento {
         this.nombre = nombre;
         this.empleados = new ArrayList<>();
     }
-
+    public void addEmpleado(Empleado empleado) {
+        empleados.add(empleado);
+        empleado.setDepartamento(getNombre());
+    }
+    public String getNombre() {
+        return nombre;
+    }
+    public int getCantidadEmpleado() {
+        return empleados.size();
+    }
+    public ArrayList<Empleado> getEmpleados() {
+        return empleados;
+    }
 }
