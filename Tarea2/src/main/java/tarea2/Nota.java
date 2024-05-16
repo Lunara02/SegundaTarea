c17a243a2ccb6c73cd16a07fa98b57b9713e39ce
blokkxz_ <166780700+Lunara02@users.noreply.github.com>
2024-05-16 00:59:53 -0400

    Implementacion Nota
    Se agrego la clase nota la cual permite crear archivos txt con contenidos introducidos

--- Tarea2/src/main/java/tarea2/Nota.java
+++ Tarea2/src/main/java/tarea2/Nota.java
@@ -1,4 +1,21 @@
 package tarea2;
 
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class Nota {
+    private String contenido;
+    private String nombreArchivo;
+
+    public Nota(String contenido, String nombreArchivo) {
+        try {
+            FileWriter nota = new FileWriter(nombreArchivo);
+            nota.write(contenido);
+            nota.close();
+        } catch (IOException a) {
+            // Si ocurre un error durante la escritura del archivo, se imprime el mensaje de error en la consola.
+            a.getMessage();
+            a.printStackTrace();
+        }
+    }
 }
